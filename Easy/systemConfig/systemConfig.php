546b11283358d1019e6d414b869dfdc5abe0ff6e
Soeren <ssp@inwx.de>
2014-08-12 15:24:48 +0200

    Stuff

--- Easy/systemConfig/systemConfig.php
+++ Easy/systemConfig/systemConfig.php
@@ -1,7 +1,7 @@
 <?php
 namespace Easy\systemConfig;
 class systemConfig extends Easy\PDOW\PDOW
-{}
+{
 	public function set($name, $value)
 	{
 		$id = $this->insertID("INSERT INTO `systemconfig`(`name`, `value`) VALUES (?, ?)", array($name, $value));
@@ -15,6 +15,16 @@ class systemConfig extends Easy\PDOW\PDOW
 			return $r;
 		}
 		return false; 
+	}
+	public function getArray()
+	{
+		$r = $this->query("SELECT * FROM `systemconfig`");
+		$re = array();
+		foreach($r as $res)
+		{
+			$re[$res["name"]]=$res["value"];
+		}
+		return $re;
 	}
 
 }
